ca5767e21f291323964dc523605ada7f0fdc147d
allrivenjs <jaimeruizxdd2@gmail.com>
2023-07-05 10:32:16 -0500

    remove load .env

--- main.go
+++ main.go
@@ -5,17 +5,15 @@ import (
 	"IdeaIntuition/global"
 	"IdeaIntuition/routes"
 	"github.com/gofiber/fiber/v2"
-	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
-	"log"
 )
 
 func main() {
 	//logrus.Infof("beginning application")
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("err loading: %v", err)
-	}
+	//err := godotenv.Load()
+	//if err != nil {
+	//	log.Fatalf("err loading: %v", err)
+	//}
 	//// Inicializa la base de datos
 	global.Load(c.InitDB())
 	app := fiber.New()
