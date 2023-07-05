89403c6c4ed25e42849c6d00e6a27c2f8329e443
allrivenjs <jaimeruizxdd2@gmail.com>
2023-07-04 23:10:40 -0500

    fix(database): changue to type connect in db of railway

--- config/Database.go
+++ config/Database.go
@@ -12,9 +12,10 @@ import (
 func InitDB() *gorm.DB {
 	// Configura tus credenciales de la base de datos aquí
 	logrus.Infof("loading database config")
-	dsn := fmt.Sprintf("%s:%s@tcp(containers-us-west-143.railway.app:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
 		os.Getenv("DB_PORT"),
 		os.Getenv("DB_NAME"),
 	)
