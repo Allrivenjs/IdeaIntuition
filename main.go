6bc1e9c9174c25734ba32152cec3cfca01f47fea
allrivenjs <jaimeruizxdd2@gmail.com>
2023-07-05 12:57:16 -0500

    add cors

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"IdeaIntuition/global"
 	"IdeaIntuition/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/sirupsen/logrus"
 	"os"
 )
@@ -18,6 +19,12 @@ func main() {
 	//// Inicializa la base de datos
 	global.Load(c.InitDB())
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowHeaders:     "Origin, Content-Type, Accept, Content-Length, Accept-Language, Accept-Encoding, Connection, Access-Control-Allow-Origin",
+		AllowOrigins:     "*",
+		AllowCredentials: true,
+		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
+	}))
 	routes.SetupRoutes(app)
 	//app.Use(middlewares.RouteLogger(app))
 	port := os.Getenv("PORT")
